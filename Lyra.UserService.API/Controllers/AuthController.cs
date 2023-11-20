0376a55341e081374faf3f07ae3d053f9222fc9d
jjeffrin <jjeffrin@gmail.com>
2023-11-20 17:25:45 +0530

    add try-catch

--- Lyra.UserService.API/Controllers/AuthController.cs
+++ Lyra.UserService.API/Controllers/AuthController.cs
@@ -17,11 +17,13 @@ namespace Lyra.UserService.API.Controllers
     {
         private readonly UserDbContext _context;
         private readonly IConfiguration _configuration;
+        private readonly ILogger<AuthController> _logger;
 
-        public AuthController(UserDbContext context, IConfiguration configuration)
+        public AuthController(UserDbContext context, IConfiguration configuration, ILogger<AuthController> logger)
         {
             this._context = context;
             this._configuration = configuration;
+            this._logger = logger;
         }
 
         [HttpPost]
@@ -46,48 +48,65 @@ namespace Lyra.UserService.API.Controllers
         [Route("AccessToken")]
         public async Task<ActionResult<LoginResponse>> Login(LoginRequest request)
         {
-            var user = await this._context.Users.FirstOrDefaultAsync(x => x.Email.Equals(request.Email));
-            if (user == null) return NotFound("User not found");
-            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            try
             {
-                return BadRequest("Incorrect Password");
-            }
-            var userClaims = new List<Claim>
+                var user = await this._context.Users.FirstOrDefaultAsync(x => x.Email.Equals(request.Email));
+                if (user == null) return NotFound("User not found");
+                if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                {
+                    return BadRequest("Incorrect Password");
+                }
+                var userClaims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.Name),
                 new Claim(ClaimTypes.Email, user.Email)
             };
 
-            var securityKey = this._configuration.GetSection("Keys:SecurityKey").Value;
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey!));
-            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(
-                issuer: "Lyra", 
-                audience: "Lyra", 
-                claims: userClaims, 
-                expires: DateTime.Now.AddDays(1), 
-                signingCredentials: cred);
+                var securityKey = this._configuration.GetSection("Keys:SecurityKey").Value;
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey!));
+                var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    issuer: "Lyra",
+                    audience: "Lyra",
+                    claims: userClaims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: cred);
 
-            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+                var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
 
-            var response = new LoginResponse()
+                var response = new LoginResponse()
+                {
+                    //AccessToken = jwtToken,
+                    UserId = user.Id,
+                    Name = user.Name
+                };
+
+                HttpContext.Response.Cookies.Append("lyra_access_token", jwtToken, new CookieOptions()
+                {
+                    SameSite = SameSiteMode.None,
+                    Secure = true,
+                    HttpOnly = true,
+                    Path = "/",
+                    Domain = null,
+                    Expires = DateTime.Now.AddDays(1)
+                });
+
+                var userDetails = JsonConvert.SerializeObject(new { name = user.Name, userId = user.Id }, Formatting.Indented);
+                HttpContext.Response.Cookies.Append("lyra_user", userDetails, new CookieOptions()
+                {
+                    SameSite = SameSiteMode.None,
+                    Secure = true,
+                    Path = "/",
+                    Domain = null,
+                    Expires = DateTime.Now.AddDays(1)
+                });
+
+                return Ok(response);
+            }
+            catch (Exception ex)
             {
-                //AccessToken = jwtToken,
-                UserId = user.Id,
-                Name = user.Name
-            };
-
-            HttpContext.Response.Cookies.Append("lyra_access_token", jwtToken, new CookieOptions() 
-            { 
-                SameSite = SameSiteMode.None, Secure = true, HttpOnly = true, Path="/", Domain = null, Expires = DateTime.Now.AddDays(1) 
-            });
-
-            var userDetails = JsonConvert.SerializeObject(new { name = user.Name, userId = user.Id }, Formatting.Indented);
-            HttpContext.Response.Cookies.Append("lyra_user", userDetails, new CookieOptions() { 
-                SameSite = SameSiteMode.None, Secure = true, Path="/", Domain = null, Expires = DateTime.Now.AddDays(1)
-            });
-
-            return Ok(response);
+                this._logger.LogError(ex.Message);
+            }            
         }
     }
 }
