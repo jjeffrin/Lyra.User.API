e01daceda42638c9e879bf36684755d8547a6457
jjeffrin <jjeffrin@gmail.com>
2023-11-21 10:40:32 +0530

    enable swagger in prod

--- Lyra.UserService.API/Program.cs
+++ Lyra.UserService.API/Program.cs
@@ -28,11 +28,8 @@ builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapHealthChecks("/health");
 app.UseHttpsRedirection();
